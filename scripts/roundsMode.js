e0ea8effba34a473c30faf9ce9f05ca25930c2c1
pushpars-shivani <pushpars@oregonstate.edu>
2026-04-26 11:56:47 -0700

    Implement confirmDelete for round deletion with confirmation dialog #11

--- scripts/roundsMode.js
+++ scripts/roundsMode.js
@@ -504,4 +504,43 @@ if (e.code === "Tab" && document.activeElement == GlobalRoundsModeLogCancelBtn &
     e.preventDefault();
     return;
 }
+}  GlobalRoundDateErr.classList.remove("hidden");
+  GlobalRoundDateErr.focus();
+  GlobalFirstFocusableLogRoundItem.set(GlobalRoundDateErr);
+} else {
+  GlobalRoundDateErr.classList.add("hidden");
+}
+});
+
+/*************************************************************************
+* @function keyDownRoundDialogFocused 
+* @desc 
+* When the user presses a key with an element in the Log Round 
+* dialog focused, we implement the accessible keyboard interface for
+* a modal dialog box. This means that "Escape" dismisses the dialog and
+* that it is impossible to tab outside of the dialog box.
+* @global GlobalFirstFocusableLogRoundItem: References the first focusable
+*         item in "Log Round" dialog. 
+* @global GlobalRoundsModeLogCancelBtn: The "Cancel" button (last focusable 
+*         item in "Log Round" dialog)
+*************************************************************************/
+function keyDownRoundDialogFocused(e) {
+if (e.code === "Escape") {
+  GlobalRoundsModeLogCancelBtn.click();
+  return;
+}
+if (e.code === "Tab" && document.activeElement == GlobalFirstFocusableLogRoundItem.get() &&
+   e.shiftKey) {
+    //shift focus to last focusable item in dialog
+    GlobalRoundsModeLogCancelBtn.focus();
+    e.preventDefault();
+    return;
+}
+if (e.code === "Tab" && document.activeElement == GlobalRoundsModeLogCancelBtn &&
+    !e.shiftKey) {
+    //shift focus to first focusable item in dialog
+    GlobalFirstFocusableLogRoundItem.get().focus();
+    e.preventDefault();
+    return;
+}
 }
